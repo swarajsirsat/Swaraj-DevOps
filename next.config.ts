950e7db3762c3e247faec8d5f4a5a9761344b2b1
swarajsirsat <85947589+swarajsirsat@users.noreply.github.com>
2025-11-18 04:58:32 -0800

    Configure output for Docker standalone build
    Added output option for Docker standalone build.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // Required for Docker standalone build
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
     remotePatterns: [
       {
